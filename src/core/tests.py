776a88b467c30c58d0114f415db5568c982ea64f
Cristian Fabricio Rodriguez Romero <fab7696650@hotmail.com>
2014-08-09 13:25:29 -0500

    Primer test

--- src/core/tests.py
+++ src/core/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from core.models import *
+
+# Create your tests here.
+
+class OfertaTest(TestCase):
+    def insertar(self):
+        Oferta.objects.create(empresa="codetag", salario=134780, funciones="nada", beneficios="ninguno")
+        Oferta.objects.create(empresa="juanito", salario=2342341234, funciones="todo", beneficios="eps y otros")
+    
+    def test_oferta(self):
+        self.insertar()
+        empresa1 = Oferta.objects.get(empresa="codetag")
+        empresa2 = Oferta.objects.get(empresa="juanito")
+        self.assertEqual(empresa1.empresa, "codetag")
 
 # Create your tests here.
