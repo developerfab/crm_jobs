2c9bdb1602a6fe40029fed3e4bbc8c8f1370924a
jefree <jgarzon920429@gmail.com>
2014-08-09 16:18:53 -0500

    refactor landing

--- src/crm/landing/views.py
+++ src/crm/landing/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-	
-	return render(request, "base_landing.html")
+	return render(request, "home.html")
+
+def faq(request):
+	return render(request, "faq.html")
