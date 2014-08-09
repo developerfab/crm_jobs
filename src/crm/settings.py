c039b54662e3d99b615aed309834c9004536a87e
Miguel Gutierrez <ryuuzakiupldr@gmail.com>
2014-08-09 15:08:45 -0500

    estilos e imagenes del landing

--- src/crm/settings.py
+++ src/crm/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crm.landing',
 )
 
 MIDDLEWARE_CLASSES = (
--- src/crm/urls.py
+++ src/crm/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'crm.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+	url(r'^', include('crm.landing.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
