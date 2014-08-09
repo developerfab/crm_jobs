0b625065a8712f41373ec3b84891dce0bb13d457
Miguel Gutierrez <ryuuzakiupldr@gmail.com>
2014-08-09 15:16:08 -0500

    capetas landing

---
+++ src/crm/landing/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+
+
+urlpatterns = patterns('crm.landing.views',
+    # Examples:
+    # url(r'^$', 'crm.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'home', name='landing'),
+)
