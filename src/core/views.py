2e5407ef376f26aa23c888a1e26d76905ef24da8
Cristian Fabricio Rodriguez Romero <fab7696650@hotmail.com>
2014-08-09 15:27:37 -0500

    Adicion de la barra de busqueda en el templade de buscar_ofertas

--- src/core/views.py
+++ src/core/views.py
@@ -3,15 +3,17 @@ from django.template import RequestContext
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render_to_response, get_object_or_404
-from core.models import *
+from core.models import * 
+from django.db.models import Q
 
 
 # Create your views here.
 def buscar_ofertas(request):
     try:
         busqueda = request.POST['palabras']
-        ofertas = Oferta.objects.all().filter(tecnologia__contains=busqueda)
+        ofertas = Oferta.objects.all().filter(Q(tecnologia__contains=busqueda) | Q(tecnologia__contains=busqueda))
     except:
+        print("excepcion")
         ofertas = Oferta.objects.all()  
 
     paginator = Paginator(ofertas, 25) 
