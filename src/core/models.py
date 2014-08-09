92b8f4c7476a2d7f0a3ebb738211444e0f0ee90d
alejo0317 <alejandro.0317@gmail.com>
2014-08-09 12:56:11 -0500

    Agregado modelo de Oferta y hecha prueba

--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Oferta (models.Model):
+	empresa = models.CharField(max_length=100)
+	salario = models.DecimalField(max_digits=12, decimal_places=2)
+	funciones = models.TextField()
+	beneficios = models.TextField()
+
+	class Meta:
+		verbose_name_plural = "Ofertas"
+
+	def __unicode__ (self):
+		return u"%s -  %s" % (empresa, str(salario))
+
