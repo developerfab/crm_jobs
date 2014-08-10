0d01fd6f85a4594d6ff83c5f05a6b203b6b3da70
alejo0317 <alejandro.0317@gmail.com>
2014-08-10 03:02:12 -0500

    anadidas migraciones de south

---
+++ src/crm/landing/migrations/0001_initial.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'ContactUser'
+        db.create_table(u'landing_contactuser', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('kind', self.gf('django.db.models.fields.IntegerField')()),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75)),
+            ('phone', self.gf('django.db.models.fields.CharField')(max_length=30, null=True, blank=True)),
+        ))
+        db.send_create_signal(u'landing', ['ContactUser'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'ContactUser'
+        db.delete_table(u'landing_contactuser')
+
+
+    models = {
+        u'landing.contactuser': {
+            'Meta': {'object_name': 'ContactUser'},
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'kind': ('django.db.models.fields.IntegerField', [], {}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '30', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['landing']
