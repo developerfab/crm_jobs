22b57f8afca07ab3abbbbc347c8459931f40007b
alejo0317 <alejandro.0317@gmail.com>
2014-08-09 23:35:20 -0500

    Añadida tecnologias a oferta y desarollador

--- src/core/models.py
+++ src/core/models.py
@@ -2,40 +2,56 @@
 from django.db import models
 from taggit.managers import TaggableManager
 from django.contrib.auth.models import User
-from django.contrib.auth.models import (
-    BaseUserManager, AbstractBaseUser
-)
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
+NIVELES_DESARROLLADOR = (
+        (0, u'Junior'),
+        (1, u'Semisenior'),
+        (2, u'Senior'),
+    )
+
+class Tecnologia (models.Model):
+    nombre = models.CharField(max_length=100)
+    def __unicode__(self):
+        return u"%s" % (self.nombre)
 
 class Desarrollador(models.Model):
     user = models.OneToOneField(User) 
     telefono = models.CharField(max_length=13)
-    tecnologias = models.TextField()
     perfil_github = models.URLField()
     perfil_linkedin = models.URLField()
     perfil_bitbucked = models.URLField()
     perfil_twitter = models.URLField()
-
-
-    USERNAME_FIELD = 'email'
+    tecnologias = models.ManyToManyField(Tecnologia, through= 'TecnologiaDesarrollador')
 
     class Meta:
         verbose_name_plural = "Desarrolladores"
 
-
 class Oferta (models.Model):
+    nombre = models.CharField(max_length=100)
     empresa = models.CharField(max_length=100)
     salario = models.DecimalField(max_digits=12, decimal_places=2)
     funciones = models.TextField()
     beneficios = models.TextField()
-    tecnologias = models.TextField()
     estado = models.BooleanField(default=True)
     fecha = models.DateTimeField(auto_now_add=True, blank=True)
     tags = TaggableManager()
     aplicantes = models.ManyToManyField(Desarrollador)
+    tecnologias = models.ManyToManyField(Tecnologia, through= 'TecnologiaOferta')
 
     class Meta:
         verbose_name_plural = "Ofertas"
 
     def __unicode__(self):
         return u"%s -  $%s" % (self.empresa, str(self.salario))
+
+class TecnologiaOferta(models.Model):
+    tecnologia  = models.ForeignKey(Tecnologia)
+    nivel = models.IntegerField(choices=NIVELES_DESARROLLADOR)
+    oferta = models.ForeignKey(Oferta)
+
+class TecnologiaDesarrollador(models.Model):
+    desarrollador = models.ForeignKey(Desarrollador)
+    tecnologia  = models.ForeignKey(Tecnologia)
+    nivel = models.IntegerField(choices=NIVELES_DESARROLLADOR)
+    
