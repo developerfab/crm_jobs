747517287c3616d209274aa5d9795991b8861a1f
alejo0317 <alejandro.0317@gmail.com>
2014-08-09 23:35:20 -0500

    Añadida tecnologias a oferta y desarollador

--- src/core/admin.py
+++ src/core/admin.py
@@ -3,6 +3,20 @@
 from django.contrib import admin
 from core.models import *
 
+class TecnologiaDesarrolladorInline(admin.TabularInline):
+    model = TecnologiaDesarrollador
+
+class DesarrolladorAdmin(admin.ModelAdmin):
+	inlines = [TecnologiaDesarrolladorInline]
+
+class TecnologiaDesarrolladorInline(admin.TabularInline):
+    model = TecnologiaDesarrollador
+
+class OfertaAdmin(admin.ModelAdmin):
+	inlines = [TecnologiaDesarrolladorInline]
+
 # Register your models here.
-admin.site.register(Oferta)
-admin.site.register(Desarrollador)
+admin.site.register(Oferta, OfertaAdmin)
+admin.site.register(Desarrollador, DesarrolladorAdmin)
+admin.site.register(Tecnologia)
+
--- src/core/models.py
+++ src/core/models.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from taggit.managers import TaggableManager
-from django.contrib.auth.models import (
-    BaseUserManager, AbstractBaseUser
-)
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+
+NIVELES_DESARROLLADOR = (
+        (0, u'Junior'),
+        (1, u'Semisenior'),
+        (2, u'Senior'),
+    )
+
+
+class Tecnologia (models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return u"%s" % (self.nombre)
 
 
 class Desarrollador(AbstractBaseUser):
+
     email = models.EmailField(
         verbose_name='Correo',
         max_length=255,
@@ -15,10 +27,11 @@ class Desarrollador(AbstractBaseUser):
 
     nombre = models.CharField(max_length=100)
     telefono = models.CharField(max_length=13)
-    tecnologias = models.TextField()
 
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
+
+    tecnologias = models.ManyToManyField(Tecnologia, through= 'TecnologiaDesarrollador')
 
     USERNAME_FIELD = 'email'
 
@@ -43,18 +56,30 @@ class Desarrollador(AbstractBaseUser):
 
 
 class Oferta (models.Model):
+    nombre = models.CharField(max_length=100)
     empresa = models.CharField(max_length=100)
     salario = models.DecimalField(max_digits=12, decimal_places=2)
     funciones = models.TextField()
     beneficios = models.TextField()
-    tecnologias = models.TextField()
     estado = models.BooleanField(default=True)
     fecha = models.DateTimeField(auto_now_add=True, blank=True)
     tags = TaggableManager()
     aplicantes = models.ManyToManyField(Desarrollador)
+    tecnologias = models.ManyToManyField(Tecnologia, through= 'TecnologiaOferta')
 
     class Meta:
         verbose_name_plural = "Ofertas"
 
     def __unicode__(self):
         return u"%s -  $%s" % (self.empresa, str(self.salario))
+
+class TecnologiaOferta(models.Model):
+    tecnologia  = models.ForeignKey(Tecnologia)
+    nivel = models.IntegerField(choices=NIVELES_DESARROLLADOR)
+    oferta = models.ForeignKey(Oferta)
+
+class TecnologiaDesarrollador(models.Model):
+    desarrollador = models.ForeignKey(Desarrollador)
+    tecnologia  = models.ForeignKey(Tecnologia)
+    nivel = models.IntegerField(choices=NIVELES_DESARROLLADOR)
+    
